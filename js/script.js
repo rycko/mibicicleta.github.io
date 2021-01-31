c31c0891620f20d36a94b4278a8c061979a52949
BerserkRycko <rycko.arcr@gmail.com>
2021-01-30 21:15:18 -0600

    30 de enero 2021 21:15

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function resizeHeaderOnScroll() {
+  const distanceY = window.pageYOffset || document.documentElement.scrollTop,
+  shrinkOn = 200,
+  headerEl = document.getElementById('js-header');
+  
+  if (distanceY > shrinkOn) {
+    headerEl.classList.add("smaller");
+  } else {
+    headerEl.classList.remove("smaller");
+  }
+}
+
+window.addEventListener('scroll', resizeHeaderOnScroll);
